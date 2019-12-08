e284aeaa205ccece2f75d7fff41f9481d5addd4e
Виталий <vitalij.baev.98@bk.ru>
2019-12-08 11:34:19 +0300

    сделал меню

--- src/assets/js/components/menu.js
+++ src/assets/js/components/menu.js
@@ -1,17 +1,29 @@
 let menuButton = document.getElementById('js-header__burger');
 let menu = document.getElementById('js-menu');
 let header = document.getElementById('js-header');
+let headerIntro = document.getElementById("js-header__intro");
 
-header.style.background = "background: url('../../assets/img/header/bg-menu.png')";
 
-console.log(header);
 
-menuButton.addEventListener('click', navigation);
 
-function navigation(){
-    if(menu.style.display === 'none'){
+menuButton.addEventListener('click', function(){
+    if(menu.style.display === "none") {
+
         menu.style.display = 'block';
+        headerIntro.style.display = "none";
+
+        header.style.background = "url('assets/img/header/bg-menu.png')";
+        header.style.backgroundPosition =  "center";
+        header.style.backgroundRepeat =  "no-repeat";
+        header.style.backgroundSize =  "cover";
     }else{
-        menu.style.display = 'none'
+
+        menu.style.display = 'none';
+        headerIntro.style.display = "flex";
+
+        header.style.background = "url('assets/img/header/header-bg.jpg')";
+        header.style.backgroundPosition =  "center";
+        header.style.backgroundRepeat =  "no-repeat";
+        header.style.backgroundSize =  "cover";
     }
-}
+})
