22d41a2d70f2f5b91800750cf79a6a844ab49103
SinkP <arseniiminiasy@gmail.com>
2019-12-12 09:42:13 +0300

    Added animation

--- src/assets/js/components/menu.js
+++ src/assets/js/components/menu.js
@@ -1,45 +1,4 @@
-<<<<<<< HEAD
-=======
-// Так делать НЕ НУЖНО
->>>>>>> d7e8b16565f556ba924da8a4406f236b5a0b6f01
-// let menuButton = document.getElementById('js-header__burger');
-// let menu = document.getElementById('js-menu');
-// let header = document.getElementById('js-header');
-// let headerIntro = document.getElementById("js-header__intro");
-<<<<<<< HEAD
-//
-=======
->>>>>>> d7e8b16565f556ba924da8a4406f236b5a0b6f01
-
-
-//
-// menuButton.addEventListener('click', function(){
-//     if(menu.style.display === "none") {
-//
-//         menu.style.display = 'block';
-//         headerIntro.style.display = "none";
-//
-//         header.style.background = "url('assets/img/header/bg-menu.png')";
-//         header.style.backgroundPosition =  "center";
-//         header.style.backgroundRepeat =  "no-repeat";
-//         header.style.backgroundSize =  "cover";
-//     }else{
-//
-//         menu.style.display = 'none';
-//         headerIntro.style.display = "flex";
-//
-//         header.style.background = "url('assets/img/header/header-bg.jpg')";
-//         header.style.backgroundPosition =  "center";
-//         header.style.backgroundRepeat =  "no-repeat";
-//         header.style.backgroundSize =  "cover";
-//     }
-// })
-
-// menuButton.addEventListener('click', function(){
-//     if(menu.style.display === "none") {
-
-<<<<<<< HEAD
-const options = [
+let options = [
     {
         open: document.querySelector('.header__burger'),
         target: document.querySelector('.menu'),
@@ -55,52 +14,11 @@ const options = [
         class: 'text-show',
     },
     {
-        open: document.querySelector('.header__close'),
-        target: document.querySelector('.header__close'),
-        close: document.querySelector('.header__burger'),
-        class: 'hidden',
-        state: true,
-    },
-    {
         open: document.querySelector('.header__burger'),
-        target: document.querySelector('.header__burger'),
-        close: document.querySelector('.header__close'),
-        class: 'hidden',
-        state: false,
-    }
-];
-
-for (const option of options) {
-    toggle(option)
-}
-
-=======
-//         menu.style.display = 'block';
-//         headerIntro.style.display = "none";
-
-//         header.style.background = "url('assets/img/header/bg-menu.png')";
-//         header.style.backgroundPosition =  "center";
-//         header.style.backgroundRepeat =  "no-repeat";
-//         header.style.backgroundSize =  "cover";
-//     }else{
-
-//         menu.style.display = 'none';
-//         headerIntro.style.display = "flex";
-
-//         header.style.background = "url('assets/img/header/header-bg.jpg')";
-//         header.style.backgroundPosition =  "center";
-//         header.style.backgroundRepeat =  "no-repeat";
-//         header.style.backgroundSize =  "cover";
-//     }
-// })
-// Так делать НУЖНО
-const options = [
-    {
-        open: document.querySelector('.header__burger'),
-        target: document.querySelector('.menu'),
+        target: document.querySelector('.menu__list-bg'),
         close: document.querySelector('.header__close'),
         state: false,
-        class: 'menu--opened',
+        class: 'text-show--big',
     },
     {
         open: document.querySelector('.header__close'),
@@ -117,29 +35,54 @@ const options = [
         state: false,
     }
 ]
+
+
+for (const item of document.getElementsByClassName('menu__list-item-link')) {
+    options.push(
+        {
+            open: document.querySelector('.header__burger'),
+            target: item,
+            close: document.querySelector('.header__close'),
+            state: false,
+            class: 'text-show',
+        }
+    )
+}
+for (const item of document.getElementsByClassName('menu__list-bg')) {
+    options.push(
+        {
+            open: document.querySelector('.header__burger'),
+            target: item,
+            close: document.querySelector('.header__close'),
+            state: false,
+            class: 'text-show--big',
+        }
+    )
+}
+
+for (const item of document.getElementsByClassName('menu__list-img')) {
+    options.push(
+        {
+            open: document.querySelector('.header__burger'),
+            target: item,
+            close: document.querySelector('.header__close'),
+            state: false,
+            class: 'text-show--img',
+        }
+    )
+}
+
 for (const option of options) {
     toggle(option)
 }
-
-
-
->>>>>>> d7e8b16565f556ba924da8a4406f236b5a0b6f01
 
 function toggle(options) {
     options.open.addEventListener('click', function () {
         options.state = true
         options.target.classList.add(options.class)
-<<<<<<< HEAD
-    });
-=======
     })
->>>>>>> d7e8b16565f556ba924da8a4406f236b5a0b6f01
     options.close.addEventListener('click', function () {
         options.state = false
         options.target.classList.remove(options.class)
     })
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> d7e8b16565f556ba924da8a4406f236b5a0b6f01
